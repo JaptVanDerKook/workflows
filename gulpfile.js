8314eca4618cdce3c0c71cd87ee70ca9e40f9c7b
Adam van der Steeg <Adam.vanderSteeg@derivco.co.za>
2016-12-01 16:25:11 +0200

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 //gulp-util, allows outputting sometihing to the console with a method called LOG().
-var gulp = require ('gulp');
-var gulptil = require ('gulp-util');
+var gulp = require ('gulp'),
+    gulptil = require ('gulp-util'),
+    coffee = require ('gulp-coffee');
 
-gulp.task('log', function(){
-    gulptil.log('PRINT TO CONSOLE TEST');
+var coffeeSrouces = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function(){
+    gulp.src(coffeeSrouces)
+    // the results from the gulp.src will be passed into the .pipe
+    .pipe(coffee({ bare: true})
+         .on('error', gulptil.log)) // If you don't have this .on('error) it will crash gulp if you get an error.
+    .pipe(gulp.dest('components/scripts')) // this will move the results of the coffee command to this method
 });
